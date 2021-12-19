a28dbf1a695c969eb09452b9381495d916c41e99
NirajSrivastava18 <nirajsrivastava2001@gmail.com>
2021-12-19 20:15:00 +0530

    Final App

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,7 +4,8 @@ import { Typography,Row,Col,Statistic } from 'antd';
 import {Link} from "react-router-dom"
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
-import {Cryptocurrencies,News} from "../components"
+import Cryptocurrencies from './Cryptocurrencies';
+import News from './News';
 
 const {Title} = Typography
 
@@ -17,7 +18,7 @@ const Homepage = () => {
         <>
            <Title level = {2} className="heading">Global Crypto Stats</Title> 
            <Row>
-               <Col span = {12}><Statistic title = "Total Cryptocurrcies" value = {globalStats.total} /></Col>
+               <Col span = {12}><Statistic title = "Total Cryptocurrcies " value = {globalStats.total}/></Col>
                <Col span = {12}><Statistic title = "Total Exchanges" value = {millify(globalStats.totalExchanges)} /></Col>
                <Col span = {12}><Statistic title = "Total MarketCap"  value = {millify(globalStats.totalMarketCap)} /></Col>
                <Col span = {12}><Statistic title = "Total 24h Volume"  value = {millify(globalStats.total24hVolume)} /></Col>
@@ -29,10 +30,10 @@ const Homepage = () => {
                    <Title level={3} className ="show-more"><Link to="/cryptocurrencies">Show More</Link></Title>
                    
            </div>
-           <Cryptocurrencies / >
+           <Cryptocurrencies simplified / >
            <div className="home-heading-container">
                    <Title level={2} className ="home-title">Latest Crypto News</Title>
-                   <Title level={3} className ="show-more"><Link to="/cryptocurrencies">Show More</Link></Title>
+                   <Title level={3} className ="show-more"><Link to="/news">Show More</Link></Title>
                    
            </div>
            <News simplified / >
